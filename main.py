f46fa4bb57512f25e35039033e48e3daba6468a6
StylahAspects <>
2021-05-18 02:47:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,61 @@
+from tkinter import *
+from PIL import Image, ImageTk
+names = []
+
+
+class Quiz:
+    def __init__(self, parent):#constructor, The __init__() function is called automatically every time the class is being used to create a new object.
+ 
+        background_color="OldLace"
+        self.bg_image = PhotoImage(file="road.png") 
+        #frame set up
+        self.quiz_frame=Frame(parent, bg = background_color)
+        self.quiz_frame.grid()#This geometry manager organizes widgets in a table-like structure in the parent widget.
+               
+        #widgets goes below
+        self.heading_label=Label(self.quiz_frame, text="NZ Road Rules", font=("Tw Cen MT","18","bold"),bg=background_color)
+        self.heading_label.grid(row=0) 
+         #label for image
+        self.image_label= Label(self.quiz_frame, image=self.bg_image)
+        self.image_label.grid(row=0, column=1) #on right side
+
+      
+        #label for username
+        self.user_label=Label(self.quiz_frame, text="Please enter your username below: ", font=("Tw Cen MT","16"),bg=background_color)
+        self.user_label.grid(row=1) 
+        
+        #entry box
+        self.entry_box=Entry(self.quiz_frame)
+        self.entry_box.grid(row=2,padx=20, pady=20)
+        
+        #continue button
+        self.continue_button = Button(self.quiz_frame, text="Continue", font=("Helvetica", "13", "bold"), bg="green", command=self.name_collection)
+        self.continue_button.grid(row=3, padx=20, pady=20)        
+       
+
+    def name_collection(self):
+        name=self.entry_box.get()
+        names.append(name) #add name to names list declared at the beginning
+        self.quiz_frame.destroy() #Destroy name frame then open the quiz runner
+      
+           
+
+if __name__ == "__main__":
+    root = Tk()
+    root.title("NZ Road Rules Quiz") 
+
+    quiz_instance = Quiz(root) #instantiation, making an instance of the class Quiz
+    root.mainloop()#so the frame doesnt dissapear
+ 
+    
+"""Whenever the Python interpreter reads a source file, it does two things:
+
+it sets a few special variables like __name__, and then
+it executes all of the code found in the file.
+
+If you are running your module (the source file) as the main program, e.g. guiQuizPart1.py
+the interpreter will assign the hard-coded string "__main__" to the __name__ variable
+
+https://www.youtube.com/watch?v=sugvnHA7ElY
+
+"""
